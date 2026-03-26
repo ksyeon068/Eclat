97f386aef6d24449cef81bb23dbc6d6a73f44e6d
seyeon <ksyeon068@gmail.com>
2026-03-26 17:34:29 +0900

    디테일바텀추가중

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+
+import DetailBottom from './DetailBottom';
+
 import { MdOutlineStarOutline } from "react-icons/md";
 
 const Detail = () => {
     return (
         <div>
             <MdOutlineStarOutline />
+            <DetailBottom />
         </div>
     );
 };
